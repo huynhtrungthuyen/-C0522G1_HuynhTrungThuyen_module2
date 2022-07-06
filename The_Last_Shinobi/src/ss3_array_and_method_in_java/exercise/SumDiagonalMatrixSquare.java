7ed36d301ee08ee29ab16985ea60dbb3fcea4aba
huynhtrungthuyen <huynhtrungthuyen@gmail.com>
2022-07-06 13:47:46 +0700

    oop

--- The_Last_Shinobi/src/ss3_array_and_method_in_java/exercise/SumDiagonalMatrixSquare.java
+++ The_Last_Shinobi/src/ss3_array_and_method_in_java/exercise/SumDiagonalMatrixSquare.java
@@ -8,15 +8,15 @@ public class SumDiagonalMatrixSquare {
         System.out.println("Nhập số hàng của Ma trận vuông: ");
         int size = scanner.nextInt();
         double[][] matrixSquare = new double[size][size];
-        for (int i=0; i<size; i++) {
-            for (int j=0; j<size; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 System.out.printf("matrixSquare[%d][%d]", i, j);
                 matrixSquare[i][j] = scanner.nextDouble();
             }
         }
 
         double sum = 0;
-        for (int i=0; i<size; i++) {
+        for (int i = 0; i < size; i++) {
             sum += matrixSquare[i][i];
         }
         System.out.println("Tổng các số ở đường chéo chính của ma trận vuông là: " + sum);
